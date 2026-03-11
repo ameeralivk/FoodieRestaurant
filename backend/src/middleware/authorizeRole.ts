9287380f47e9d086ff269603f183812d911b067d
ameer ali vk <ameervk1875@gamil.com>
2026-03-11 12:53:43 +0530

    authentication fixed

--- backend/src/middleware/authorizeRole.ts
+++ backend/src/middleware/authorizeRole.ts
@@ -1,15 +1,38 @@
 import { Request, Response, NextFunction } from "express";
 import { MESSAGES } from "../constants/messages";
 
+// export const authorizeRoles = (
+//   ...allowedRoles: string[]
+// ): ((req: Request, res: Response, next: NextFunction) => void) => {
+//   return (req, res, next) => {
+//     const user = (req as any).user;
+//     if (!user || !allowedRoles.includes(user.role)) {
+//       res.status(403).json({ message: MESSAGES.ADMIN_UNAUTHERIZED_ERROR });
+//       return;
+//     }
+//     next();
+//   };
+// };
+
 export const authorizeRoles = (
   ...allowedRoles: string[]
 ): ((req: Request, res: Response, next: NextFunction) => void) => {
   return (req, res, next) => {
     const user = (req as any).user;
-    if (!user || !allowedRoles.includes(user.role)) {
+
+    if (!user) {
       res.status(403).json({ message: MESSAGES.ADMIN_UNAUTHERIZED_ERROR });
       return;
     }
+
+    const userRole = user.role?.toLowerCase();
+    const roles = allowedRoles.map(role => role.toLowerCase());
+
+    if (!roles.includes(userRole)) {
+      res.status(403).json({ message: MESSAGES.ADMIN_UNAUTHERIZED_ERROR });
+      return;
+    }
+
     next();
   };
 };
